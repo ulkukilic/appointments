b7b8f7bf9a4116a9a979319b16f9c24a52572b64
ulkukilic <ukilic1@stu.vistula.edu.pl>
2025-05-21 10:49:57 +0200

    css

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login Page</title>
-    <link href="{{ asset('assets/css/style.css') }}" rel="stylesheet">
+    <link href="{{ asset('panel/assets/css/style.css') }}" rel="stylesheet">
 
 </head>
 <body>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Register Page</title>
-     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+     <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
 <!--   css dosyasinin konumu belirtlendi HTML DE  "href=style.css" oldugu gibi-->
 </head>
 <body>
